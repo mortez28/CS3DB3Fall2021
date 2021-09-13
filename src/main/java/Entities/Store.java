c1b39696c5c892339ba5051062a6ce557c4dddf2
mortez28 <morteza.alipur@gmail.com>
2021-09-12 23:20:10 -0400

    New relationship

--- src/main/java/Entities/Store.java
+++ src/main/java/Entities/Store.java
@@ -6,4 +6,10 @@ public class Store {
     public String Description;
     public String Page;
     public String StartDate;
+
+    public String OwnerFirstName;
+    public String OwnerLastName;
+    public String OwnerDoB;
+
+
 }
---
+++ src/main/java/Relationships/StoreEmployeeWorks.java
@@ -0,0 +1,11 @@
+package Relationships;
+
+public class StoreEmployeeWorks {
+
+    public int StoreID;
+
+    public String EmployeeFirstName;
+    public String EmployeeLastName;
+    public String EmployeeDoB;
+
+}
